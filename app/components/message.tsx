8a8e4d8692b0ba40d708e7527d5e6ecf7611b133
shadsvm <91400290+shadsvm@users.noreply.github.com>
2024-11-13 01:01:24 +0100

    ➕ add a dependency.

---
+++ app/components/message.tsx
@@ -0,0 +1,110 @@
+'use client'
+import { useEffect, useRef } from 'react'
+import anime from 'animejs'
+
+export default function Message() {
+  const messages = ['Hi there, looking for dev?', 'Let me build your ideas']
+  const bubbles = useRef<HTMLDivElement[]>([])
+  const text = useRef<HTMLParagraphElement[]>([])
+  const dot = useRef<HTMLSpanElement[]>([])
+  const loadingSpeed = 600
+
+  useEffect(() => {
+    if (!bubbles.current?.length) return
+
+    const timeline = anime.timeline({
+      easing: 'easeInOutSine',
+    })
+
+    bubbles.current.forEach((bubble: HTMLDivElement, index: number) => {
+      if (!text.current?.length || !dot.current?.length) return
+
+      const message = text.current[index]
+      const loading = dot.current[index]
+
+      timeline
+        .add({
+          targets: bubble,
+          easing: 'easeOutSine',
+          duration: 200,
+          opacity: 1,
+          scale: [0, 1],
+          translateX: [150, 0],
+        })
+        .add({
+          targets: '.dot',
+          opacity: [
+            { value: 0.6, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 1, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 0.6, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 1, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 0.6, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 1, easing: 'easeOutSine', duration: loadingSpeed },
+          ],
+          scale: [
+            { value: 0.7, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 1, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 0.7, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 1, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 0.7, easing: 'easeOutSine', duration: loadingSpeed },
+            { value: 1, easing: 'easeOutSine', duration: loadingSpeed },
+          ],
+          delay: anime.stagger(300, {
+            grid: [1, 3],
+            from: 'first',
+          }),
+        })
+        .add({
+          targets: bubble,
+          borderBottomRightRadius: ['0px', '9999px'],
+          width: [loading.offsetWidth + 40, message.offsetWidth + 40],
+          height: [loading.offsetHeight + 20, message.offsetHeight + 10],
+          easing: 'easeOutSine',
+          duration: 200,
+          begin: function () {
+            loading.style.display = 'none'
+            message.style.color = 'black'
+          },
+        })
+        .add({
+          targets: message,
+          opacity: [0, 1],
+          duration: 200,
+          easing: 'easeOutSine',
+        })
+    })
+    timeline.play()
+  }, [bubbles, text, dot])
+
+  return messages.map((item, index) => (
+    <div
+      key={index}
+      ref={(el) => {
+        if (el) bubbles.current[index] = el
+      }}
+      className="bubble opacity-0 relative"
+    >
+      <p
+        ref={(el) => {
+          if (el) text.current[index] = el
+        }}
+        className="message text-white!"
+      >
+        {item}
+      </p>
+      <span
+        ref={(el) => {
+          if (el) dot.current[index] = el
+        }}
+        className="loading flex gap-1.5 sm:gap-2 absolute"
+      >
+        {[...Array(3)].map((_, i) => (
+          <span
+            key={i}
+            className="dot w-2 h-2 sm:w-2.5 sm:h-2.5 rounded-full bg-black"
+          ></span>
+        ))}
+      </span>
+    </div>
+  ))
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,65 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import Message from './components/message'
+import { useEffect } from 'react'
+import anime from 'animejs'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const links = [
+    {
+      name: 'Github',
+      href: 'https://github.com/shadsvm',
+    },
+  ]
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  useEffect(() => {
+    anime({
+      targets: 'section',
+      translateY: ['50', 0],
+      opacity: [0, 1],
+      delay: anime.stagger(250),
+    })
+  }, [])
+
+  return (
+    <main className="max-w-3xl mx-auto h-[100dvh] flex flex-col justify-center items-center  gap-5 px-3 sm:px-5">
+      <section className="w-full flex justify-start items-center gap-4 sm:gap-6 ">
+        <Image
+          width="75"
+          height="75"
+          src="/memoji-wink.png"
+          alt="Cartoon-style picuter of me"
+          className="bg-white/20 shadow-lg rounded-full max-w-20"
+        />
+
+        <header>
+          <h1 className="text-xl sm:text-4xl font-semibold ">
+            Samuel Kaminski
+          </h1>
+          <h2 className="dark:text-stone-300 sm:text-lg">
+            Full Stack Developer
+          </h2>
+        </header>
+      </section>
+
+      <section className="text-stone-500 text-xs text-justify tracking-wider px-4">
+        driven by a passion for technology and a love for bringing ideas to
+        life. I guide products from concept to launch with a thoughtful,
+        hands-on approach.
+      </section>
+      <section className="w-full flex gap-2 flex-wrap ">
+        {links.map((link) => (
+          <Link key={link.href} href={link.href}>
+            {link.name}
+          </Link>
+        ))}
+      </section>
+
+      <section className="flex flex-col gap-2 justify-start  items-end w-full">
+        <Message />
+      </section>
+    </main>
+  )
 }
