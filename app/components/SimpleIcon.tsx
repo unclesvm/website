c3c0f7f57b509e38357d49436e2596730d95a52f
shadsvm <91400290+shadsvm@users.noreply.github.com>
2025-01-27 10:42:47 +0100

    💄 add or update the ui and style files.

--- app/components/SimpleIcon.tsx
+++ app/components/SimpleIcon.tsx
@@ -1,13 +1,15 @@
+import { ComponentProps } from "react"
+
 interface Icon {
   name: string
   showLabel?: boolean
 }
 
-export default function SimpleIcon({name, showLabel}: Icon) {
+export default function SimpleIcon({ name, showLabel }: Icon & ComponentProps<'div'>) {
   return (
-    <div key={name} className="text-sm text-neutral-800 dark:text-neutral-200 flex justify-start items-center gap-2">
+    <div key={name} className={"text-neutral-800 dark:text-neutral-200 flex justify-start items-center gap-2"}>
       <img height="12" width="12" src={`https://cdn.simpleicons.org/${name}/black/AAAAAA`} alt={name} />
-      {showLabel && <div className="">{name}</div>}
+      {showLabel && <div className="text-xs">{name}</div>}
     </div>
   )
 }
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,19 +1,19 @@
 import SimpleIcon from "./SimpleIcon"
 
 const content = {
-  "Programming languages": ["css", "html5", "javascript", "typescript", "python"],
+  "Programming languages": ["html5", "css", "javascript", "typescript", "python"],
   "Frameworks": ["react", "next.js", "vue.js", "nuxt", "astro", "hono"],
   "Tools": ["git", "tailwindcss", "redux", "prisma", "docker", "firebase"],
-  "JS Runtime": ["node.js", "bun"],
+  "Runtime": ["node.js", "bun"],
 }
 
 export default function Skills() {
   return (
     <div className="space-y-10 ">
       {Object.entries(content).map(([key, value]) => (
-        <div key={key} className="border-b border-gray-800">
-          {key}
-            <div className="flex justify-start items-center h-10 uppercase gap-8">
+        <div key={key} className="border-b border-neutral-200 dark:border-neutral-700">
+          <strong>{key}</strong>
+            <div className="flex justify-normal gap-10 items-center h-10 uppercase">
               {value.map((name) =>
                 <SimpleIcon key={name} name={name} showLabel />
               )}
