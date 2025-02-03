d4e405ca91f026af3801952f02adb9b2162450d6
shadsvm <91400290+shadsvm@users.noreply.github.com>
2025-02-03 15:03:53 +0100

    ➕ add a dependency.

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,23 +1,50 @@
-import SimpleIcon from "./SimpleIcon"
+import { Icon } from '@iconify/react';
 
-const content = {
-  "Programming languages": ["html5", "css", "javascript", "typescript", "python"],
-  "Frameworks": ["react", "next.js", "vue.js", "nuxt", "astro", "hono"],
-  "Tools": ["git", "tailwindcss", "redux", "prisma", "docker", "firebase"],
-  "Runtime": ["node.js", "bun"],
-}
+const content = [
+  [
+    {name: "Languages"},
+    {name: 'html5', label: 'html'},
+    {name: 'css'},
+    {name: 'javascript'},
+    {name: 'typescript'},
+    {name: 'python'}
+  ],
+  [
+    {name: "Frameworks"},
+    {name: 'react'},
+    {name: 'nextdotjs', label: 'next'},
+    {name: 'vuedotjs', label: 'vue'},
+    {name: 'nuxtdotjs', label: 'nuxt'},
+    {name: 'astro'},
+    {name: 'hono'}
+  ],
+  [
+    {name: "Tools"},
+    {name: 'git'},
+    {name: 'tailwindcss', label: 'tailwind'},
+    {name: 'redux'},
+    {name: 'prisma'},
+    {name: 'docker'},
+    {name: 'firebase'}
+  ],
+  [
+    {name: "Runtime"},
+    {name: 'nodedotjs', label: 'node'},
+    {name: 'bun'}
+  ],
+]
 
 export default function Skills() {
   return (
-    <div className="space-y-10 ">
-      {Object.entries(content).map(([key, value]) => (
-        <div key={key} className="border-b border-neutral-200 dark:border-neutral-700">
-          <strong>{key}</strong>
-            <div className="flex justify-normal gap-10 items-center h-10 uppercase">
-              {value.map((name) =>
-                <SimpleIcon key={name} name={name} showLabel />
-              )}
+    <div className="flex border border-neutral-600 dark:border-neutral-500 rounded divide-x">
+      {content.map((entry, index) => (
+        <div key={index + entry.toString()} className=" flex flex-col justify-start divide-neutral-200 dark:divide-neutral-800 divide-y items-start  uppercase  border-neutral-200 dark:border-neutral-700 max-w-xs w-full">
+          {Object.values(entry).map(({name, label}) =>
+            <div key={name} className='first:font-bold first:text-sm tracking-wide text-xs w-full flex justify-start items-center !p-3 gap-2 '>
+              <Icon icon={'simple-icons:' + name} className='w-3 h-3' />
+              <div className=''>{label || name}</div>
             </div>
+          )}
         </div>
       ))}
     </div>
--- app/resume/layout.tsx
+++ app/resume/layout.tsx
@@ -1,7 +1,7 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
-    <div className="p-5 my-12 mx-auto prose dark:prose-invert prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-lg prose-h6:text-base dark:prose-headings:text-white">
+    <div className="p-5 my-12 mx-auto prose dark:prose-invert prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-base prose-h6:text-sm dark:prose-headings:text-white">
       {children}
     </div>
   )
